c31f1636de3b5bde8ce58dde7b721f3cdd72c037
Pavel <pavel@opinary.com>
2021-09-27 15:57:44 +0300

    upgrade gradle version; add more urls to test

--- app/src/main/java/com/example/webviewactivitytest/MainActivity.kt
+++ app/src/main/java/com/example/webviewactivitytest/MainActivity.kt
@@ -15,7 +15,9 @@ class MainActivity : AppCompatActivity() {
         title = "KotlinApp"
         val webView = findViewById<WebView>(R.id.webView)
         webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://www.tagesspiegel.de")
+        webView.loadUrl("https://preview-m.tagesspiegel.de/berlin/zu-viele-woelfe-brandenburgs-bauernverband-befuerwortet-jagd-auf-woelfe-und-erntet-kritik/27522818.html")
+//        webView.loadUrl("https://clients.opinary.com/test-pages/test-iframes/page-standard-ifame.html")
+//        webView.loadUrl("https://clients.opinary.com/test-pages/test-iframes/page-with-blob-iframe.html")
 //        webView.loadUrl("https://clients.opinary.com/xandr_sponsored_ams_placeholder/editorial-test.html")
         val webSettings = webView.settings
         webSettings.javaScriptEnabled = true
