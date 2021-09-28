c4733addeb2e76b17dd5eaefb218f7fef8aa0e6a
Pavel <pavel@opinary.com>
2021-09-28 18:51:51 +0300

    enable debug mode for the webview

--- app/src/main/java/com/example/webviewactivitytest/MainActivity.kt
+++ app/src/main/java/com/example/webviewactivitytest/MainActivity.kt
@@ -14,13 +14,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         title = "KotlinApp"
         val webView = findViewById<WebView>(R.id.webView)
+        WebView.setWebContentsDebuggingEnabled(true);
         webView.webViewClient = WebViewClient()
         webView.loadUrl("https://preview-m.tagesspiegel.de/berlin/zu-viele-woelfe-brandenburgs-bauernverband-befuerwortet-jagd-auf-woelfe-und-erntet-kritik/27522818.html")
 //        webView.loadUrl("https://clients.opinary.com/test-pages/test-iframes/page-standard-ifame.html")
 //        webView.loadUrl("https://clients.opinary.com/test-pages/test-iframes/page-with-blob-iframe.html")
 //        webView.loadUrl("https://clients.opinary.com/xandr_sponsored_ams_placeholder/editorial-test.html")
         val webSettings = webView.settings
-        webSettings.javaScriptEnabled = true
+        webSettings.javaScriptEnabled = true;
     }
 
     override fun onBackPressed() {
